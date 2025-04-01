71bbfbe022f0bcc3c2100bcdb9326e13137d8e3c
r-keenan <ross.keenan@protonmail.comm>
2025-04-01 12:30:09 -0500

    added flowbyte header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import NavBar from '$lib/components/NavBar.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen flex-col">
+	<NavBar />
+	<main class="flex-grow pt-2.5">{@render children()}</main>
+</div>
