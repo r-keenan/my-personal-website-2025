82686dfa3c86bc87431173258ff90416494a7f7c
r-keenan <ross.keenan@protonmail.comm>
2025-04-01 11:37:22 -0500

    moved tailwind config in next.js to app.css in this repo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	/*
 	export let data;
 	const { sanityData } = data;
 	console.log(sanityData);
+*/
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-blue-light">Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
