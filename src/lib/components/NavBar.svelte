16f36d08236387d7ddfd09a8b4680fee41276163
r-keenan <ross.keenan@protonmail.comm>
2025-04-01 15:19:51 -0500

    created styling simliar to 2021 navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -2,7 +2,9 @@
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
-<Navbar>
+<Navbar
+	class="background-blue-dark background-blur-sm fixed top-0 z-10 w-full text-white opacity-90"
+>
 	<NavBrand href="/">
 		<!--
 		<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -13,10 +15,10 @@
 	</NavBrand>
 	<NavHamburger />
 	<NavUl>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/posts">Posts</NavLi>
-		<NavLi href="/skills">Skills</NavLi>
-		<NavLi href="/about">About Me</NavLi>
-		<NavLi href="/contact">Contact</NavLi>
+		<NavLi href="/" class="text-blue-light">Home</NavLi>
+		<NavLi href="/posts" class="text-blue-light">Posts</NavLi>
+		<NavLi href="/skills" class="text-blue-light">Skills</NavLi>
+		<NavLi href="/about" class="text-blue-light">About Me</NavLi>
+		<NavLi href="/contact" class="text-blue-light">Contact</NavLi>
 	</NavUl>
 </Navbar>
