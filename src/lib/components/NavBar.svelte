463cc521250ff84e3deb8df00cfa1fc3ce403391
r-keenan <ross.keenan@protonmail.com>
2025-04-07 19:36:26 -0500

    removed deprecated app/stores. derived value is the new way to track active url link

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
-	$: activeUrl = $page.url.pathname;
-	const activeClass =
+
+	const activeUrl = $derived(page.url.pathname);
+
+	let activeClass =
 		'text-white-DEFAULT bg-green-100 md:bg-transparent md:text-white-DEFAULT md:dark:text-white dark:bg-green-600 md:dark:bg-transparent';
-	const nonActiveClass =
+	let nonActiveClass =
 		'text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
 </script>
 
