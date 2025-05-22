c77b897c3e0e203fa8a4f4342e320463e80a9489
r-keenan <ross.keenan@protonmail.com>
2025-05-21 21:28:20 -0500

    updated flowbite svelte package. fixed styling on mobile navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -11,32 +11,34 @@
 		'text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-300 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
 </script>
 
-<Navbar class="bg-blue-dark/80 fixed top-0 z-10 w-full text-white backdrop-blur-md">
-	<NavBrand href="/">
-		<!--
+<header>
+	<Navbar class="bg-blue-dark/80 fixed top-0 z-10 w-full text-white backdrop-blur-md">
+		<NavBrand href="/" class="pl-2.5">
+			<!--
 		<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
 		-->
-		<span
-			class="shake self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-			id="myNameHeader">Ross Keenan</span
-		>
-	</NavBrand>
-	<div class="flex items-center md:order-2">
-		<Avatar id="avatar-menu" src={avi} />
-		<Button
-			size="sm"
-			class="bg-blue-light text-white-DEFAULT hover:bg-blue-dark hover:text-white-DEFAULT ml-4.5"
-			><a href="/contact"> Contact Me </a></Button
-		>
-		<NavHamburger />
-	</div>
-	<NavUl {activeUrl} {activeClass} {nonActiveClass}>
-		<NavLi href="/" class="text-blue-light hover:bg-gray-light hover:text-white-DEFAULT">Home</NavLi
-		>
-		<NavLi href="/posts" class="text-blue-light hover:bg-gray-700 hover:text-white">Posts</NavLi>
-		<NavLi href="/skills" class="text-blue-light hover:bg-gray-700 hover:text-white">Skills</NavLi>
-		<NavLi href="/about" class="text-blue-light hover:bg-gray-700 hover:text-white">About Me</NavLi>
-		<NavLi href="/contact" class="text-blue-light hover:bg-gray-700 hover:text-white">Contact</NavLi
-		>
-	</NavUl>
-</Navbar>
+			<span
+				class="shake self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+				id="myNameHeader">Ross Keenan</span
+			>
+		</NavBrand>
+		<div class="flex items-center md:order-2">
+			<Avatar id="avatar-menu" src={avi} />
+			<NavHamburger />
+		</div>
+		<NavUl {activeUrl} {activeClass} {nonActiveClass}>
+			<NavLi href="/" class="text-blue-light hover:bg-gray-light hover:text-white-DEFAULT"
+				>Home</NavLi
+			>
+			<NavLi href="/posts" class="text-blue-light hover:bg-gray-700 hover:text-white">Posts</NavLi>
+			<NavLi href="/skills" class="text-blue-light hover:bg-gray-700 hover:text-white">Skills</NavLi
+			>
+			<NavLi href="/about" class="text-blue-light hover:bg-gray-700 hover:text-white"
+				>About Me</NavLi
+			>
+			<NavLi href="/contact" class="text-blue-light hover:bg-gray-700 hover:text-white"
+				>Contact</NavLi
+			>
+		</NavUl>
+	</Navbar>
+</header>
