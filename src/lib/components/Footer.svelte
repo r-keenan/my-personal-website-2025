398e55eca78bdc9c86b0719fa9ed147443f70f3b
r-keenan <ross.keenan@protonmail.com>
2025-05-23 10:44:17 -0500

    prototyped a footer and added it to the layout

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,37 @@
+<script>
+	import {
+		Footer,
+		FooterCopyright,
+		FooterLinkGroup,
+		FooterLink,
+		FooterIcon
+	} from 'flowbite-svelte';
+	import { GithubSolid, LinkedinSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Footer footerType="logo">
+	<div class="sm:flex sm:items-center sm:justify-between">
+		<FooterLinkGroup
+			class="mb-6 flex flex-wrap items-center text-sm text-gray-500 sm:mb-0 dark:text-gray-400"
+		>
+			<FooterLink href="/">About</FooterLink>
+			<FooterLink href="/">Privacy Policy</FooterLink>
+			<FooterLink href="/">Licensing</FooterLink>
+			<FooterLink href="/">Contact</FooterLink>
+		</FooterLinkGroup>
+	</div>
+	<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
+	<FooterCopyright href="/" by="Ross Keenan" />
+	<div class="mt-4 flex space-x-6 pt-3 sm:mt-0 sm:justify-center rtl:space-x-reverse">
+		<FooterIcon href="https://github.com/r-keenan">
+			<GithubSolid
+				class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+			/>
+		</FooterIcon>
+		<FooterIcon href="https://www.linkedin.com/in/r-keenan">
+			<LinkedinSolid
+				class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+			/>
+		</FooterIcon>
+	</div>
+</Footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import NavBar from '$components/NavBar.svelte';
+	import Footer from '$components/Footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -8,4 +9,5 @@
 <div class="flex min-h-screen flex-col">
 	<NavBar />
 	<main class="flex-grow">{@render children()}</main>
+	<Footer />
 </div>
