fec0fe08b49ba103d7bed4372726be3cf8fd69d7
r-keenan <ross.keenan@protonmail.com>
2025-05-27 13:59:39 -0500

    Adjusted max height of image on post page and content width.

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -10,16 +10,16 @@
 	<div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full">
 		<div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true"></div>
 	</div>
-	<div class="relative mb-10 px-14 sm:mb-20 sm:px-6 md:mb-32 lg:mb-40 lg:px-8 lg:py-5">
+	<div class="relative mb-10 px-4 sm:mb-20 sm:px-6 md:mb-32 lg:mb-40 lg:px-8 lg:py-5">
 		<Breadcrumb title={post.title} slug={post.slug.current} />
-		<div class="mx-auto max-w-prose text-lg">
+		<div class="mx-auto max-w-4xl">
 			<div class="pt-24">
 				<img
-					class="rounded-md object-cover"
+					class="h-48 w-full rounded-md object-cover"
 					src={post.imageUrl?.toString()}
 					alt=""
-					width={1239}
-					height={576}
+					width="413"
+					height="192"
 				/>
 			</div>
 			<h1>
@@ -41,38 +41,29 @@
 				</time>
 			</div>
 		</div>
-		<div class="grid">
-			<div class="grid-1 items-center justify-center">
-				<div
-					class="prose prose-indigo prose-lg text-gray-medium lg:mx-2xl:mx-auto mt-10 grid sm:mx-20 2xl:w-1/3"
-				>
-					{#each post.body as postBody (postBody._key)}
-						<div class="grid w-full grid-cols-1">
-							<div class="w-full">{postBody.children[0].text}</div>
-							<br />
-						</div>
-					{/each}
-				</div>
+		<div class="mx-auto mt-10 max-w-4xl">
+			<div class="prose prose-indigo prose-lg text-gray-medium mx-auto">
+				{#each post.body as postBody (postBody._key)}
+					<div class="mb-4">
+						<p>{postBody.children[0].text}</p>
+					</div>
+				{/each}
+			</div>
 
-				<div class="my-10">
-					<div class="mt-10 grid grid-cols-1 justify-items-center">
-						<a
-							href={post.url?.toString()}
-							class="text-blue-light text-base font-medium"
-							target="_blank"
-							rel="noreferrer"
-						>
-							{post.urlDescription}
-						</a>
-					</div>
-				</div>
-				<div class="my-1">
-					<div class="grid grid-cols-1 justify-items-center">
-						<a href="/posts" class="text-blue-light text-base font-medium">
-							<span aria-hidden="true"> &larr;</span> Back to Posts
-						</a>
-					</div>
-				</div>
+			<div class="my-10 text-center">
+				<a
+					href={post.url?.toString()}
+					class="text-blue-light text-base font-medium"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{post.urlDescription}
+				</a>
+			</div>
+			<div class="my-1 text-center">
+				<a href="/posts" class="text-blue-light text-base font-medium">
+					<span aria-hidden="true"> &larr;</span> Back to Posts
+				</a>
 			</div>
 		</div>
 	</div>
