db7676eaaa8b70c0640731d4a7aea25787dd2f1d
r-keenan <ross.keenan@protonmail.com>
2025-05-27 14:36:26 -0500

    updated with prettier format.

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h2', async ({ page }) => {
 	await page.goto('/');
-	
+
 	// Wait for the page to load and check for the specific h2 content
 	await expect(page.locator('h2:has-text("Senior Software Consultant")')).toBeVisible();
 });
