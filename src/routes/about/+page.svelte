76919ad6b2f8d4b9749ba73616cf64b8c86398ee
r-keenan <ross.keenan@protonmail.com>
2025-05-27 09:53:22 -0500

    drafted new layout for about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,44 +3,37 @@
 </script>
 
 <div>
-	<div class="relative min-h-screen bg-white py-8 sm:py-20 lg:pt-50 lg:pb-72">
-		<div class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:items-start lg:gap-24 lg:px-8">
-			<div class="relative sm:py-16 lg:py-0">
-				<div
-					class="relative mx-auto max-w-md px-10 py-4 sm:max-w-max sm:px-6 lg:max-w-max lg:px-16 lg:py-20"
-				>
-					<div class="grid justify-items-center overflow-hidden rounded-2xl shadow-xl">
-						<img
-							class="absolute inset-0 h-full w-full rounded-2xl object-cover"
-							src={aboutMePic2}
-							alt="Me"
-						/>
-					</div>
+	<div class="relative min-h-screen bg-white py-8 pt-28 sm:py-20 sm:pt-32">
+		<div class="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
+			<!-- Avatar Image Section -->
+			<div class="my-8 flex justify-center sm:mb-12">
+				<div class="h-64 w-64 rounded-2xl shadow-xl sm:h-80 sm:w-80">
+					<img class="h-full w-full rounded-2xl object-contain" src={aboutMePic2} alt="Me" />
 				</div>
 			</div>
-			<div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
-				<div class="pt-12 sm:pt-16 lg:pt-20">
-					<h2 class="text-gray-dark text-3xl font-extrabold tracking-tight sm:text-4xl">
-						Ross Keenan
-					</h2>
-					<h3 class="text-gray-dark pt-2 text-2xl font-bold tracking-tight sm:text-2xl">
-						Senior Software Consultant
-					</h3>
-					<div class="text-gray-medium mt-6 space-y-6">
-						<p class="text-base leading-7">
-							I develop in everything from Content Management Systems (CMS) to web sites and web
-							apps. I am interested in everything from the hot, new JavaScript frameworks, to
-							backend code, databases, and data engineering.
-						</p>
-						<p class="text-base leading-7">
-							Feel free to reach out to me for any consultations or sites that you want to develop
-							or update. I am not looking for full-time employment, but I am open to outside
-							contract work. I am available to hire for hourly or fixed-priced contracts.
-						</p>
-						<p class="text-base leading-7">
-							<span>Let&apos;s make something great together!</span>
-						</p>
-					</div>
+
+			<!-- Content Section -->
+			<div class="text-center">
+				<h2 class="text-gray-dark text-3xl font-extrabold tracking-tight sm:text-4xl">
+					Ross Keenan
+				</h2>
+				<h3 class="text-gray-dark pt-2 text-2xl font-bold tracking-tight sm:text-2xl">
+					Senior Software Consultant
+				</h3>
+				<div class="text-gray-medium mx-auto mt-6 max-w-3xl space-y-6">
+					<p class="text-base leading-7">
+						I develop in everything from Content Management Systems (CMS) to web sites and web apps.
+						I am interested in everything from the hot, new JavaScript frameworks, to backend code,
+						databases, and data engineering.
+					</p>
+					<p class="text-base leading-7">
+						Feel free to reach out to me for any consultations or sites that you want to develop or
+						update. I am not looking for full-time employment, but I am open to outside contract
+						work. I am available to hire for hourly or fixed-priced contracts.
+					</p>
+					<p class="text-base leading-7">
+						<span>Let&apos;s make something great together!</span>
+					</p>
 				</div>
 				<div class="mt-10">
 					<div>
