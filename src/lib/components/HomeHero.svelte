cf497560f78ecd1a33b52ba134c1086e2e0774c8
r-keenan <ross.keenan@protonmail.com>
2025-06-09 14:22:06 -0500

    shorted to my website for animated text.

--- src/lib/components/HomeHero.svelte
+++ src/lib/components/HomeHero.svelte
@@ -5,7 +5,7 @@
 <div class="heroContainer">
 	<span>
 		<p class="heroText break-words">
-			<span id="nvimText">nvim</span> my-personal-website
+			<span id="nvimText">nvim</span> my-website
 		</p>
 	</span>
 	<img src={mainBackground} alt="Header Background" class="heroImage" />
