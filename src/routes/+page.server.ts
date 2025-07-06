14a8732f62a3c34c338b27b874ce9179ef458952
r-keenan <ross.keenan@protonmail.com>
2025-07-06 18:29:03 -0500

    added SSG for this page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { getPostsPreview, getQualifications, sanityApiStore } from '$lib/stores/sanityDataStore';
 
-// This will not allow me to use a constant from utils. It throws an error that the value must be statically analyzable
-// 86400 = seconds in a day
+export const prerender = true;
 
 export const load: PageServerLoad = async () => {
 	const state = sanityApiStore.get();
