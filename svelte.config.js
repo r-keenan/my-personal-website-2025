1cdefb2a692a05d3ea0391113f0bdb39d8822d9f
r-keenan <ross.keenan@protonmail.com>
2025-07-20 10:29:21 -0500

    updated svelte config to disable prerendering in testing.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
+import { preprocess } from 'svelte/compiler';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +13,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		prerender: {
+			enabled: process.env.CI !== 'true' && process.env.NOD_ENV !== 'test'
+		},
 		adapter: adapter({
 			runtime: 'nodejs22.x'
 		}),
