d24fd24aa0ea7cfcb45925fd40f09db15cd53439
r-keenan <ross.keenan@protonmail.com>
2025-07-21 12:51:43 -0500

    removed loading data on the layout, since I am moving away from the store.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,2 @@
-import { getQualifications } from '$lib/utils/repositories/sanityRepo';
-import type { Qualification } from '$lib/utils/types/types';
-
 export const prerender = true;
 export const csr = true;
-
-export const load = async () => {
-	const data: Qualification[] = await getQualifications();
-
-	return {
-		initialData: {
-			qualifications: data?.[0] || []
-		}
-	};
-};
