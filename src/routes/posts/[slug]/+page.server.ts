7be51eb6aff843d6e91a27237156a9c5d2f15001
r-keenan <ross.keenan@protonmail.com>
2025-07-21 12:55:08 -0500

    simplified data pulling in slug post page

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,32 +1,16 @@
+import { sanityClient } from '$lib/clients/sanity';
 import type { EntryGenerator, PageServerLoad } from './$types';
-import {
-	getPost,
-	getPostsPreview,
-	getQualifications,
-	sanityApiStore
-} from '$lib/stores/sanityDataStore';
 
 export const load: PageServerLoad = async ({ params, depends }) => {
 	depends(`posts:${params.slug}`);
-	const state = sanityApiStore.get();
-	const promises = [];
 	const { slug } = params;
 
-	if (!state.qualifications?.data) {
-		promises.push(getQualifications());
-	}
-	if (!state.postsPreview?.data) {
-		promises.push(getPostsPreview());
-	}
-	promises.push(getPost(slug));
-
-	if (promises.length > 0) {
-		await Promise.all(promises);
-	}
+	const client = await sanityClient();
+	const data = await client.fetch(slug);
 
 	return {
 		initialData: {
-			post: sanityApiStore.get().post.data || []
+			post: data || []
 		}
 	};
 };
