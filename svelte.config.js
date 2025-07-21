dbe20a49433215593c7fe3f44e389b7c0b7fc7d3
r-keenan <ross.keenan@protonmail.com>
2025-07-21 10:25:17 -0500

    removed deprecated enabled option and switched to http option

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		prerender: {
-			enabled: process.env.CI !== 'true' && process.env.NODE_ENV !== 'test'
+			handleHttpError: ({ path, referrer, message }) => {
+				if (process.env.CI === 'true' || process.env.NODE_ENV === 'test') {
+					console.warn(`Prerender error ignored: ${path} - ${message}`);
+					return;
+				}
+				throw new Error(message);
+			}
 		},
 		adapter: adapter({
 			runtime: 'nodejs22.x'
