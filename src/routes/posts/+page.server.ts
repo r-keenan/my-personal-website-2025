2573a4fa640080532687fc029201c56e89a6a02d
r-keenan <ross.keenan@protonmail.com>
2025-07-21 12:47:47 -0500

    simplified get all post previews

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -1,28 +1,13 @@
 import type { PageServerLoad } from './$types';
-import { getPostsPreview, getQualifications, sanityApiStore } from '$lib/stores/sanityDataStore';
-
-// This will not allow me to use a constant from utils. It throws an error that the value must be statically analyzable
-// 86400 = seconds in a day
-//export const revalidate = 86_400;
+import type { PostPreview } from '$lib/utils/types/types';
+import { getPostsPreview } from '$lib/utils/repositories/sanityRepo';
 
 export const load: PageServerLoad = async () => {
-	const state = sanityApiStore.get();
-	const promises = [];
-
-	if (!state.qualifications?.data) {
-		promises.push(getQualifications());
-	}
-	if (!state.postsPreview?.data) {
-		promises.push(getPostsPreview());
-	}
-
-	if (promises.length > 0) {
-		await Promise.all(promises);
-	}
+	const data: PostPreview[] = await getPostsPreview();
 
 	return {
 		initialData: {
-			postsPreview: sanityApiStore.get().postsPreview.data || []
+			postsPreview: data?.[0] || []
 		}
 	};
 };
