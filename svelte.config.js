28b5914760b8ef32373981fc6dafefe38d4fb9bd
r-keenan <ross.keenan@protonmail.com>
2025-07-21 10:08:23 -0500

    fixed typo in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		prerender: {
-			enabled: process.env.CI !== 'true' && process.env.NOD_ENV !== 'test'
+			enabled: process.env.CI !== 'true' && process.env.NODE_ENV !== 'test'
 		},
 		adapter: adapter({
 			runtime: 'nodejs22.x'
