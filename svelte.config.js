46c47140536141eee0f4262eaa241d879da3273b
r-keenan <ross.keenan@protonmail.com>
2025-09-10 20:03:17 -0500

    added entries to keep contact page as SPA mode

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		prerender: {
+			entries: ['*'],
 			handleHttpError: ({ path, message }) => {
 				if (process.env.CI === 'true' || process.env.NODE_ENV === 'test') {
 					console.warn(`Prerender error ignored: ${path} - ${message}`);
